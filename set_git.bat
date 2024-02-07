b6e74082870e32290d0593e9206e3476851a3576
unknown <shorok.heeb.99@gmail.com>
2024-02-07 10:56:28 +0200

    Wed 02-07-24

--- set_git.bat
+++ set_git.bat
@@ -8,5 +8,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/ShorokHeib/BTC-USD-Prices.git
+git remote add origin https://github.com/ShorokHeib/CryptoCurrency-Ex4.git
 git push -u origin main
